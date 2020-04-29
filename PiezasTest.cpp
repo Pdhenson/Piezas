e55fd615ba52327475227a5d09e8d5ab46a8699d
darius <T1ckleT@ckle>
2020-04-29 12:32:38 -0700

    Typo corrected

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -21,7 +21,7 @@ TEST(PiezasTest, sanityCheck)
 TEST(PiezasTest, makeBoardTest)
 {
   Piezas game;
-  for (int i = 0; i < 3); i++)
+  for (int i = 0; i < 3; i++)
   {
     for (int j = 0; j < 4; j++)
     {
